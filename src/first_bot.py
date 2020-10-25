e8f1184322d9a9e48f9fcc8242bf1b90fae25902
Kyle Ng <kyleecng@gmail.com>
2020-10-24 19:51:26 -0700

    Created src and test directories

--- src/first_bot.py
+++ src/first_bot.py
@@ -21,6 +21,18 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
+poll_emojis = {
+    1:"1️⃣",
+    2:"2️⃣",
+    3:"3️⃣",
+    4:"4️⃣",
+    5:"5️⃣",
+    6:"6️⃣",
+    7:"7️⃣",
+    8:"8️⃣",
+    9:"9️⃣"
+}
+
 #Load in bot auth and server tokens
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -58,6 +70,31 @@ async def create_poll(ctx, question: str, option_1: str, option_2: str):
     embedded = await ctx.send(embed=poll_embed)
     await embedded.add_reaction(emoji="1️⃣")
     await embedded.add_reaction(emoji="2️⃣")
+
+@bot.command(name="create_big_poll", help="Create a poll with more than 2 options")
+async def create_big_poll(ctx, *args):
+    question = args[0]
+    options = args[1:]
+
+    if len(options) > 9:
+        await ctx.send("Error: please use less than 9 options for your poll")
+        return
+    if len(options) < 2:
+        await ctx.send("Error: please use at least two options for your poll")
+        return
+
+    poll_embed = discord.Embed(
+        title = question
+    )
+    poll_embed.set_footer(text="Choose with the reactions below!")
+
+    for i in range(1,len(options)+1):
+        poll_embed.add_field(name=poll_emojis[i]+ " " + options[i-1], value="\u200b", inline=False)
+    
+    embedded = await ctx.send(embed=poll_embed)
+
+    for i in range(1, len(options)+1):
+        await embedded.add_reaction(emoji=poll_emojis[i])
 
 
 bot.run(TOKEN)
